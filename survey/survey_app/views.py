79e6f2783ae4983cbbcdaed948e0e42ca7ceb47b
Dave Hogue <thedavehogue@gmail.com>
2017-07-27 11:48:22 -0600

    add some layout details and push to the back burner

--- survey/survey_app/views.py
+++ survey/survey_app/views.py
@@ -3,4 +3,3 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'survey_app/home.html')
-    return HttpResponse("Welcome to the Giant Denver Apparel Survey!")
